d60ca898c33c381efcc5a84104ee29e52935d51e
james-ucas <james@ucas.ac.cn>
2021-04-24 21:28:46 +0800

    rayleigh_ritz uses tol argument for both xtol and gtol in bfgs

--- matrices/rayleigh_ritz.py
+++ matrices/rayleigh_ritz.py
@@ -4,7 +4,7 @@ from numpy.linalg import norm
 from local_optimisation.bfgs import bfgs
 
 
-def rayleigh_ritz(aa, tol=1e-3):
+def rayleigh_ritz(aa, tol=1e-6):
     evec0 = np.ones(aa.shape[0])
 
     def f(evec):
@@ -16,5 +16,5 @@ def rayleigh_ritz(aa, tol=1e-3):
         df0 = 2 * ((v.T @ v) * (aa @ v) - v @ (v.T @ aa @ v)) / (v.T @ v) ** 2
         return df0.flatten()
 
-    xmin = bfgs(f, evec0, df, tol)
+    xmin = bfgs(f, evec0, df, tol, tol)
     return f(xmin), xmin / norm(xmin)
